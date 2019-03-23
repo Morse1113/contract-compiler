054e25f0fbd1b800a548e15015a6f80dbaeb8d7e
Morse <1185585454@qq.com>
2019-03-23 18:46:38 +0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var solc = require('solc');
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express'})
+});
+
+router.post('/compile', function (req, res, next) {
+    let params = req.body;
+    console.log(params.input);
+    let output = JSON.parse(solc.compile(params.input));
+    res.json({message: 'success', data: output})
+});
+
+module.exports = router;
